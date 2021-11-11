c6b326a385b58241fddd5be59cb12f320e7efec4
volkan bulutlar <vbulutlar@hotmail.com>
2021-11-11 10:01:55 +0300

    11.11.2021 Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,42 @@
+console.log("Melis")
+
+
+
+var dolarBugun = 9.40
+
+let dolarDun =9.20
+
+
+{
+let dolarDun =9.10
+}
+
+console.log(dolarDun)
+
+
+const euroDun = 11.2
+console.log(euroDun)
+
+let konutKredileri = [" Konut Kredisi","Emlak Kredisi","Kamu Kredisi"]
+
+
+
+console.log("<ul>")
+for (let i = 0; i<konutKredileri.length; i++)
+{
+ console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+
+console.log("</ul>")
+
+{/*
+<ul>
+    <li>Konut Kredisi</li>
+    <li>Taşıt Kredisi</li>
+    <li>Kamu Kredisi</li>
+</ul>
+*/}
+
+
+console.log(konutkredileri)
